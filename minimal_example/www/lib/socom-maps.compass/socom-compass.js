01014119cdd33bdcb31622fb90865f279e84881f
Ivo Oliveira <ivo@void.pt>
2015-05-24 21:42:37 +0100

    no message

--- minimal_example/www/lib/socom-maps.compass/socom-compass.js
+++ minimal_example/www/lib/socom-maps.compass/socom-compass.js
@@ -4,63 +4,44 @@ var divcompass;
 // Compass
 // ############################
 L.Control.Compass = L.Control.extend({
-	includes: L.Mixin.Events,
-	options: {
-		position: 'topright',
-		title: 'Compass',
+    includes: L.Mixin.Events,
+    options: {
+        position: 'topright',
+        title: 'Compass'
+    },
 
-	},
+    onAdd: function (map) {
+        console.log('-----------------------------------------------');
+        this.map = map;
+        var container = L.DomUtil.create('div', 'leaflet-control-bsl');
+        this._container = container;
 
-	onAdd: function (map) {
-		this.map = map;
-		var container = L.DomUtil.create('div', 'leaflet-control-bsl');
-		this._container = container;
-		divcompass = this._container;
-		return this._container;
-	},
-	startWatch: function () {
-
-		// Update compass every 3 seconds
-		var options = {
-			frequency: 10
-		};
-
-		divcompass.style.display = 'compact';
-		watchID = navigator.compass.watchHeading(this.onSuccess,
-			this.onError, options);
-	},
-
-	// fonction appele lors du succes d'appel de la boussole
-	onSuccess: function (heading) {
-		//element.innerHTML = 'Heading: ' + heading.magneticHeading;
-		//	this.rotateCompassImage(heading);
-
-		var magneticHeading = 360 - heading.magneticHeading;
-		var rotation = "rotate(" + magneticHeading + "deg)";
-
-		divcompass.style.webkitTransform = rotation;
-	},
-
-	//fonction appele lors du l'echec d'appel de la boussole
-	onError: function (compassError) {
-		alert('Compass error: ' + compassError.code);
-	},
-	setLayer: function (_layer) {
-		if (layer) {
-			L.DomEvent.removeListener(layer, 'load', function () {
-					layer = _layer;
-					L.DomEvent.addListener(layer, 'load', this.startWatch, this);
-				}
-			);
-		} else {
-			layer = _layer;
-			L.DomEvent.addListener(layer, 'load', this.startWatch, this);
-		}
-
-	}
-
+        divcompass = this._container;
+        if (navigator.compass) {
+            this.startWatch();
+        }
+        return this._container;
+    },
+    startWatch: function () {
+        // Update compass every 3 seconds
+        var options = {
+            frequency: 3000
+        };
+        divcompass.style.display = 'compact';
+        watchID = navigator.compass.watchHeading(this.onSuccess, this.onError, options);
+    },
+    onSuccess: function (heading) {
+        //divcompass.innerHTML = 'Heading: ' + (360 - heading.magneticHeading);
+        //	this.rotateCompassImage(heading);
+        var magneticHeading = 360 - heading.magneticHeading;
+        var rotation = "rotate(" + magneticHeading + "deg)";
+        divcompass.style.webkitTransform = rotation;
+    },
+    onError: function (compassError) {
+        alert('Compass error: ' + compassError.code);
+    }
 });
 L.control.compass = function (map) {
-	return new L.Control.Compass(map);
+    return new L.Control.Compass(map);
 
 };
